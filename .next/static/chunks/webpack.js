e32426a106b525bef908420e930ea1d05e88c8d3
Nick Olivieri <NJOlivieri88@gmail.com>
2021-05-09 07:05:31 -0400

    retrieve single campaign summary

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -187,7 +187,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a54da19e6f3f76d97494"; }
+/******/ 		__webpack_require__.h = function() { return "e05841e0eae7bc3b3d85"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,19 +1,69 @@
 import React from 'react';
+import { Card } from "semantic-ui-react";
 
 import Layout from "../../components/layout";
+import campaignContract from "../../ethereum/campaign";
+import web3 from "../../ethereum/web3";
 
 const CampaignShow = props => {
+
+	const renderCards = () => {
+		const {
+			balance,
+			manager,
+			minimumContribution,
+			requestsCount,
+			approversCount
+		} = props;
+		const items = [
+			{
+				header: manager,
+				meta: 'Address of Manager',
+				description: 'The manager created this campaign and can create requests to withdraw money',
+				style: { overflowWrap: 'break-word' }
+			},
+			{
+				header: minimumContribution,
+				meta: 'Minimum Contribution (wei)',
+				description: 'You must contribute at least this much wei to become an approver',
+			},
+			{
+				header: requestsCount,
+				meta: 'Number of Requests',
+				description: 'A request tries to withdraw money from the contract.  Requests must be approved by approvers',
+			},
+			{
+				header: approversCount,
+				meta: 'Number of Approvers',
+				description: 'Number of people who have already donated to this campaign',
+			},
+			{
+				header: web3.utils.fromWei(balance, 'ether'),
+				meta: 'Campaign Balance (ether)',
+				description: 'The balance is how much money this campaign has left to spend',
+			}
+		];
+		return <Card.Group items={items} />
+	};
 
 	return (
 		<Layout>
 			<h3>Campaign Show</h3>
+			{renderCards()}
 		</Layout>
 	);
 };
 
 CampaignShow.getInitialProps = async props => {
-	console.log(props.query.address);
-	return {};
+	const campaign = campaignContract(props.query.address);
+	const summary = await campaign.methods.getSummary().call();
+	return {
+		minimumContribution: summary[0],
+		balance: summary[1],
+		requestsCount: summary[2],
+		approversCount: summary[3],
+		manager: summary[4]
+	};
 };
 
 export default CampaignShow;
